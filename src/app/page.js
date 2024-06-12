c80388b733eca4244379be7fa4539252cf8eeb68
Ritwik0710 <ritwikshri123@gmail.com>
2024-06-13 04:46:30 +0530

    photo

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
-"use client"
-import { getData } from './Firebase';
-import Login from './Login/page'
-import Button from './components/Elements/Button';
+"use client";
+import AppNavigation from "./components/Elements/Navigation/AppNavigation/page";
 export default function Page() {
-  
   return (
     <div>
-      <Login/>
-      <div onClick={() =>getData("course","AM_lab1",)}>
-      {Button("get Data","")}
+      <div>
+        <AppNavigation />
+        <text style={{ left: "200px", position: "fixed" }}>
+          Welcome to Slot Booking System
+        </text>
+        <img
+          src="/image.jpg"
+          style={{
+            width: "1000px",
+            top: "120px",
+            position: "fixed",
+            left: "200px",
+            height: "500px",
+          }}
+        />
       </div>
-      
     </div>
   );
 }
